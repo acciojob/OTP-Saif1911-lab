47836a8008a52abe29622f4b49bc0b9e519185db
Mohammed Saif <mohdsaifwork19@gmail.com>
2026-01-12 12:32:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 const codes = document.querySelectorAll(".code");
 
+function setFocused(el) {
+  codes.forEach(c => c.classList.remove("focused"));
+  el.classList.add("focused");
+  el.focus();
+}
+
+// Initial focus (important for Cypress)
+setFocused(codes[0]);
+
 codes.forEach((code, index) => {
   code.addEventListener("keyup", (e) => {
-    // Remove focused class from all
-    codes.forEach(c => c.classList.remove("focused"));
-
     if (e.key === "Backspace") {
       if (index > 0) {
-        codes[index - 1].focus();
-        codes[index - 1].classList.add("focused");
+        setFocused(codes[index - 1]);
       }
       return;
     }
 
     if (code.value && index < codes.length - 1) {
-      codes[index + 1].focus();
-      codes[index + 1].classList.add("focused");
+      setFocused(codes[index + 1]);
     }
-  });
-
-  code.addEventListener("focus", () => {
-    codes.forEach(c => c.classList.remove("focused"));
-    code.classList.add("focused");
   });
 });
